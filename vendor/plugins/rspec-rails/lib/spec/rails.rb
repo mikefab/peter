eb90fb52671a1091bbaa405c8c62665bd8c8959a
Mike Fabrikant <mikefabrikant@gmail.com>
2009-01-28 14:58:55 -0200

    adding plugins for authorization and rspec

---
+++ vendor/plugins/rspec-rails/lib/spec/rails.rb
@@ -0,0 +1,25 @@
+RAILS_ENV ||= "test"
+
+begin
+  require_dependency 'application_controller'
+rescue MissingSourceFile
+  require_dependency 'application'
+end
+
+require 'action_controller/test_process'
+require 'action_controller/integration'
+require 'active_support/test_case'
+require 'active_record/fixtures' if defined?(ActiveRecord::Base)
+
+require 'spec/test/unit'
+
+require 'spec/rails/matchers'
+require 'spec/rails/mocks'
+require 'spec/rails/example'
+require 'spec/rails/extensions'
+require 'spec/rails/interop/testcase'
+
+# This is a temporary hack to get rspec's auto-runner functionality to not consider
+# ActionMailer::TestCase to be a spec waiting to run.
+require 'action_mailer/test_case'
+Spec::Example::ExampleGroupFactory.register(:ignore_for_now, ActionMailer::TestCase)
