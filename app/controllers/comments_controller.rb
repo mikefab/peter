9ed3d1a5281fabd7ebb034a7582a23424421eef8
Mike Fabrikant <mikefabrikant@gmail.com>
2009-02-16 19:13:36 -0200

    added delete comment ability for logged in user. Added link to comments forum from comatose admin

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+  before_filter :load_post
   def create
     @post = Post.find(params[:post_id]) 
     @comment = @post.comments.create!(params[:comment])
@@ -8,4 +9,28 @@ class CommentsController < ApplicationController
       format.js
     end
   end
+
+
+  # DELETE /comments/1
+  # DELETE /comments/1.xml
+  def destroy
+
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    respond_to do |format|
+            format.html {redirect_to comments_and_forum_path }
+      format.xml  { head :ok }
+    end
+  end
+
+
+  def load_post
+
+    @post = Post.find(params[:post_id])
+    logger.debug @content.class
+
+  end
+
+
 end
